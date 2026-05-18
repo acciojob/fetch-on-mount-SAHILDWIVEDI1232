9b4adb563feb313c407622b06692f4cbbee43e32
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-18 10:36:43 +0530

    Import Posts component into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
+import Posts from "Posts.js";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+      <Posts />
     </div>
   )
 }
